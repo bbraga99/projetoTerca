3b358b4f6b036ae028faf1aa1624090df139e647
bruno henrique <0220482213011@fatectq.local>
2022-11-08 21:46:26 -0300

    Segundo commit

--- java.js
+++ java.js
@@ -12,22 +12,28 @@ salvar.addEventListener('click',function(e) {
     if(tipo != '' && marca != '' && modelo != '' && ano != '' && valor != '' ) {
         veiculo = new Veiculo(tipo,marca,modelo,ano,valor);
         listaVeiculos.push(veiculo);
+        document.querySelector('#tabela').innerHTML =(Listar(listaVeiculos));
         document.querySelectorAll('input').value = '';
     } else {
-        alert("Falta algo")
+        alert("Preencher todas as informações")
     } 
 })
 
 
 listaVeiculos = [];
 
-
 function Listar(lista) {
     let auxHtml = '';
     for ( let i = 0; i < lista.length;i++) {
-
+        auxHtml += '<tr>'+
+                        '<td>'+lista[i].tipo+'</td>'+
+                        '<td>'+lista[i].marca+'</td>'+
+                        '<td>'+lista[i].modelo+'</td>'+
+                        '<td>'+lista[i].ano+'</td>'+
+                        '<td>'+lista[i].valor+'</td>'+ 
+                    '</tr>'; 
     }
-
+    return auxHtml;
 }
 
 class Veiculo {
